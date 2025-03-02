67eabc520e60cd0a8c5fc3bbfb2c75193d1fe31b
Murali Dharan <muralidharan.radhakrishnan-extern@rntbci.com>
2025-03-02 21:12:27 +0530

    Initial commit

---
+++ pptReaderProject/src/main/java/com/tasks/pptReaderProject/PptReaderProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tasks.pptReaderProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PptReaderProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PptReaderProjectApplication.class, args);
+	}
+
+}
---
+++ pptReaderProject/src/test/java/com/tasks/pptReaderProject/PptReaderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasks.pptReaderProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PptReaderProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
